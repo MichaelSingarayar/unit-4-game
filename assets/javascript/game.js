dcf2e96067e3ce98a549f5159087ecb53daa8ead
Mike Singarayar <michaelsingarayar@gmail.com>
2018-10-28 19:08:23 -0600

    Final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,127 @@
-$(document).ready(function() {
-    
+$(document).ready(function () {
 
-    var ranNum = Math.floor((Math.random()*120)+19);
-    var gemNum = [10,1,5,8];
-    //assign one of these numbers to the gems//
-    var wins = 0;
-    var loses = 0;
-    
-    
-    
-    
-    //where do i add the two gem variables? in the global scope?//
-    
+
+    var score = 0;
+     console.log("UserScore:" + score);
+     $("#total-score").html("Total Score: " + score);
+
+
      
-    $("#red").on("click", function() {
-        
-        
-        $("#rand-num").text(ranNum);
-        console.log(this);
-        
-        
-        $("#total-score").text(gemNum);
-        console.log(gemNum);
-        
-      });
-      
+    var losses = 0;
+    console.log("Loses:" + losses);
+    $("#losses").html("Losses: " + losses);
+    
+    var win = 0;
+    console.log("Wins:" + win);
+    $("#wins").html("Wins: " + win);
+    
 
-    $("#green").on("click", function(){
-        
-        
+    ranNum = Math.floor((Math.random() * 102) + 19);
+    console.log("Random Number: " + ranNum);
+    $("#rand-num").text("Random Number: " + ranNum);
+
+    var gemRed = Math.floor((Math.random() * 11) + 1);
+    console.log("GemRed: " + gemRed);
+    $(".gemred").html("value=" + gemRed);
+
+    var gemGreen = Math.floor((Math.random() * 11) + 1);
+    console.log("GemGreen: " + gemGreen);
+    $(".gemgreen").html("value=" + gemGreen);
+
+    var gemBlue = Math.floor((Math.random() * 11) + 1);
+    console.log("GemBlue: " + gemBlue);
+    $(".gemblue").html("value=" + gemBlue);
+
+    var gemColor = Math.floor((Math.random() * 11) + 1);
+    console.log("GemColor: " + gemColor);
+    $(".gemcolor").html("value=" + gemColor);
+
+    
+
+
+
+    var gameReset = function() {    
+        score = 0;
+        ranNum = Math.floor((Math.random() * 102) + 19);
+        gemRed = Math.floor((Math.random() * 11) + 1);
+        gemGreen = Math.floor((Math.random() * 11) + 1);
+        gemBlue = Math.floor((Math.random() * 11) + 1);
+        gemColor = Math.floor((Math.random() * 11) + 1);
+        console.log("New Ran: " , ranNum);
+        $("#rand-num").html("Random Number: " + ranNum);
+        $("#total-score").html("Total Score: " + score);
+        $("#wins").html("Wins: " + win);
+        $("#losses").html("Losses: " + losses);
+    };
+
+    var gameUpdate = function() {
+        if (score == ranNum) {
+            win++;
+            console.log(win);
+            $("#wins").html("Wins: " + win);
+            gameReset();
+
+            
+            
+    
+        } else if (score > ranNum) {
+            losses++;
+            $("#losses").html("Losses: " + losses);
+            gameReset();
+        }
+    };
+
+
+    $(".gemred").on("click", function(){
+        score = score + gemRed;
+        console.log(score);
+        $("#total-score").html("Total Score: " + score)
+        gameUpdate();
+    });
+     
+     $(".gemgreen").on("click", function(){
+        score = score + gemGreen;
+        console.log(score);
+        $("#total-score").html("Total Score: " + score)
+        gameUpdate();
+
+
     });
 
-    $("#blue").on("click", function(){
-        
+    $(".gemblue").on("click", function(){
+        score = score + gemBlue;
+        console.log(score);
+        $("#total-score").html("Total Score: " + score)
+        gameUpdate();
+
+
     });
 
-    $("#color").on("click", function(){
-        
+    $(".gemcolor").on("click", function(){
+        score = score + gemColor;
+        console.log(score);
+        $("#total-score").html("Total Score: " + score)
+        gameUpdate();
+
+
     });
+
+
+
+
+    
+
+
+
+    
+
+
+
+
+
+
+
+    
 
 
 
